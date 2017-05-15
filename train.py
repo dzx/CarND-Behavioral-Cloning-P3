c843de0bae0d4e5196933ed1e26813dfb97fba4b
sasha <dzx303@gmail.com>
2017-05-15 12:41:08 -0700

    First version using generator. Augmentation needs improvement.

--- train.py
+++ train.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
        
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
+from sklearn.model_selection import train_test_split
+import sklearn
 
 
 lines = []
@@ -23,9 +25,11 @@ measurements = []
 
 FLIP_IMAGES = True
 USE_SIDE_VIEW = True
-SIDE_CORRECTION = .2
-EPOCHS = 3
+SIDE_CORRECTION = .20
+EPOCHS = 1
 DROPOUT_RATE =.4
+ZERO_ANGLE_RATE = .3
+BATCH_SIZE = 24
 
 #_show_image = True
 
@@ -38,7 +42,7 @@ def process_line(line, flip_image=False, path_index=0, correction=0):
 #        plt.imshow(image)
 #        plt.show()
     measurement = float(line[3])
-    if(measurement or (random.random() < .3)):
+    if(measurement or (random.random() < ZERO_ANGLE_RATE)):
         images.append(image)
         measurements.append(measurement + correction)
         if flip_image: # and measurement :
@@ -46,12 +50,45 @@ def process_line(line, flip_image=False, path_index=0, correction=0):
             images.append(image_flipped)
             measurements.append(-measurement)
             
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while True :
+        random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+        
+        batch_imgs = []
+        batch_angles = []
+        for batch_sample in batch_samples:
+            measurement = float(batch_sample[3])
+            img_path = batch_sample[0]
+            if (measurement or (random.random() < ZERO_ANGLE_RATE)):
+                if(random.random() < .5):
+                    measurement += SIDE_CORRECTION
+                    img_path = batch_sample[1]
+                else:
+                    measurement -= SIDE_CORRECTION
+                    img_path = batch_sample[2]
+            image = np.flip(cv.imread(img_path), 2)
+            batch_imgs.append(image)
+            batch_angles.append(measurement)
+            if FLIP_IMAGES: # and measurement :
+                image_flipped = np.fliplr(image)
+                batch_imgs.append(image_flipped)
+                batch_angles.append(-measurement)
+            
+            X_train = np.array(batch_imgs)
+            Y_train = np.array(batch_angles)
+            yield sklearn.utils.shuffle(X_train, Y_train)
+            
+                    
+
 def create_lenet(model):
     #Output = 28x28x6.
     model.add(Conv2D(6, 7, 13, activation='relu', subsample=(3, 11)))
     #Pooling. Input = 28x28x6. Output = 14x14x6.
     model.add(MaxPooling2D())
-    # Convolutional. Output = 10x10x16.
+    # Convolutional. Output = 10x10x16.['training set', 'validation set'], loc='upper right'
     model.add(Conv2D(16, 5, 5, activation='relu'))
     #Pooling. Input = 10x10x16. Output = 5x5x16.
     model.add(MaxPooling2D())
@@ -83,26 +120,37 @@ with open("./recordings/driving_logs2.csv") as logFile:
     reader = csv.reader(logFile)
     [lines.append(line) for line in reader]
 
-for i, line in enumerate(lines):
-    if not( i % 1000):
-        print("Loading image {} of {}".format(i, len(lines)))
-    process_line(line, FLIP_IMAGES)
-    if USE_SIDE_VIEW :
-        process_line(line, FLIP_IMAGES, 1, SIDE_CORRECTION)
-        process_line(line, FLIP_IMAGES, 2, -SIDE_CORRECTION)
+#for i, line in enumerate(lines):
+##    measurements.append(float(line[3]))
+#    
+#    if not( i % 1000):
+#        print("Loading image {} of {}".format(i, len(lines)))['training set', 'validation set'], loc='upper right'
+#    process_line(line, FLIP_IMAGES)
+#    if USE_SIDE_VIEW :
+#        process_line(line, FLIP_IMAGES, 1, SIDE_CORRECTION)
+#        process_line(line, FLIP_IMAGES, 2, -SIDE_CORRECTION)
 
 #clip = ImageSequenceClip(images, fps=25)
 #clip.write_videofile("training.mp4")
 
 
-X_train = np.array(images)
-del images
-y_train = np.array(measurements)
-del measurements
+#X_train = np.array(images)
+#del images
+#y_train = np.array(measurements)
+#del measurements
+#
+#fig, ax = plt.subplots()
+#ax.hist(y_train, bins=30)
+#plt.show()
 
-fig, ax = plt.subplots()
-ax.hist(y_train, bins=30)
-plt.show()
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
+epoch_sample = len(train_samples)
+epoch_val = len(validation_samples)
+if FLIP_IMAGES:
+    epoch_sample *= 2
+    epoch_val *= 2
 
 model = Sequential()
 model.add(Cropping2D(cropping=((50, 20), (0, 0)) , input_shape=(160, 320, 3)))
@@ -111,6 +159,13 @@ model = create_nvnet(model)
 #model.add(Flatten())
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, batch_size=64, nb_epoch=EPOCHS, validation_split=.2, shuffle=True)
+#model.fit(X_train, y_train, batch_size=64, nb_epoch=EPOCHS, validation_split=.2, shuffle=True)
+history_obj = model.fit_generator(train_generator, samples_per_epoch=epoch_sample, validation_data=validation_generator,
+                    nb_val_samples= epoch_val, nb_epoch=EPOCHS)
 model.save("model.h5")
 print("Model saved")
+plt.plot(history_obj.history['loss'])
+plt.plot(history_obj.history['val_loss'])
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+
